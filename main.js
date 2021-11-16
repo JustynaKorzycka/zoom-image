4e2f2b36b9f941d6801e496449a289199ee30000
Justyna Korzycka <justyna.korzycka@gmail.com>
2021-11-16 20:13:11 +0100

    add events

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let products = [];
+let ifZoom = false;
+let mouseX;
+let mouseY;
 
 const getProducts = async() => {
   try {
@@ -16,7 +19,7 @@ const createProducts = () => {
   products.forEach((product) => {
     const newProduct = document.createElement('div');
     newProduct.classList.add('card');
-    newProduct.innerHTML = `<img src="./img${product.src}" class="card-img-top" alt="${product.title}"> 
+    newProduct.innerHTML = `<div class='card-img-top'><div class='box-image' style='background-image:url("./img${product.src}")'></div></div>
     <div class="card-body">
       <p class="card-text">${product.desc} <span>${product.price}</span></p>
     </div> `;
@@ -24,6 +27,45 @@ const createProducts = () => {
   })
 }
 
+const clickOnImage = (e) => {
+  ifZoom = !ifZoom;
+  e.target.classList.toggle('zoom');
+  if (ifZoom) {
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
+  }
+}
+const changePositionOfImage = (e) => {
+  if (ifZoom) {
+    imgX = (e.clientX/2 - e.offsetX);
+    imgY = (e.clientY/2 - e.offsetY);
+
+    imgX < 0 ? imgX = 0 : imgX;
+    imgY < 0 ? imgY = 0 : imgY;
+
+    imgX > e.target.clientWidth/2  ? imgX = e.target.clientWidth/2 : imgX;
+    imgY > e.target.clientHeight ? imgY = e.target.clientHeight : imgY;
+
+     e.target.style.backgroundPosition = `${imgX}px ${imgY}px`;
+  }
+
+}
+const exitFromZoom = (e) => {
+  e.target.classList.remove('zoom');
+  ifZoom = false;
+  e.target.style.backgroundPosition = `50% 50%`;
+}
+
+const addZoom = () => {
+  const images = [...document.querySelectorAll('.box-image')];
+  images.forEach(image => {
+    image.addEventListener('click', clickOnImage);
+    image.addEventListener('mousemove', changePositionOfImage);
+    image.addEventListener('mouseout', exitFromZoom)
+  })
+}
+
 getProducts()
   .then(res => products = res)
   .then(createProducts)
+  .then(addZoom)
